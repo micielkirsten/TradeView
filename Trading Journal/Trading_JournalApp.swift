40882909660bbcaeb8f400d6dd13116d88bb3471
Kirsten P <micielkirsten@gmail.com>
2022-03-05 12:28:11 -0500

    Initial Commit

---
+++ Trading Journal/Trading_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trading_JournalApp.swift
+//  Trading Journal
+//
+//  Created by Miciel Kirsten on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Trading_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
