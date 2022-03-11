337501742bce45f8c44f6d9cf47c3447c01fe583
Kirsten P <micielkirsten@gmail.com>
2022-03-11 17:18:25 -0500

    Linked Get Started with Login Page(completed)

--- Trading Journal/Screens/GetStarted.swift
+++ Trading Journal/Screens/GetStarted.swift
@@ -9,45 +9,63 @@ import SwiftUI
 
 
 struct GetStarted: View {
+    @State var showLoginPage: Bool = false
+    
     var body: some View {
+        
+        VStack(alignment: .center){
+            Text("TradeView")
+                .font(.system(size: 70))
+                .foregroundColor(Color.white)
+                .fontWeight(.bold)
             
-        ZStack{
-            Color(#colorLiteral(red: 0.04, green: 0.09, blue: 0.05, alpha: 1.00))
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack{
-                Text("TradeView")
-                    .font(.system(size: 50))
-                    .foregroundColor(.white)
-                    .fontWeight(.bold)
-                    .position(x: 215, y: 300)
-                
-                Spacer()
-                
-                VStack{
+            Button {
+                withAnimation{
+                    showLoginPage = true
+                }
+            } label: {
+                Text("Get Started")
+                    .font(.system(size: 25))
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(#colorLiteral(red: 0.04, green: 0.09, blue: 0.05, alpha: 1.00)))
+                    .padding(.vertical, 18)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(20)
                     
-                    Button {
-                        
-                    } label: {
-                            Text("Get Started")
-                            .fontWeight(.semibold)
-                            .frame(width: 200, height: 50 ,alignment: .center)
-                            .foregroundColor(Color(#colorLiteral(red: 0.04, green: 0.09, blue: 0.05, alpha: 1.00)))
-                            .background(Color.white)
-                            .cornerRadius(20)
-                            .shadow(color: Color.black.opacity(0.1), radius: 5, x: 5, y: 5)
-                        }
-                    Spacer()
-                    }
+            }
+            .padding(.horizontal,33)
+            .offset(y: getRect().height < 750 ? 20 : 40)
+        }
+        .padding()
+        .padding(.top, getRect().height < 750 ? 0 : 20)
+        .frame(maxWidth: . infinity, maxHeight: .infinity)
+        .background(Color(#colorLiteral(red: 0.04, green: 0.09, blue: 0.05, alpha: 1.00)))
+        
+        
+        //call new page with transition
+        .overlay(
+            Group{
+                if showLoginPage{
+                    LoginPage()
+                        .transition(.move(edge: .bottom))
                 }
             }
-        }
+        )
     }
+}
 
 struct GetStarted_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             GetStarted()
         }
+    }
+}
+
+// for the display to adjust
+extension View{
+    func getRec ()-> CGRect{
+        return UIScreen.main.bounds
     }
 }
--- Trading Journal/Screens/LoginPage.swift
+++ Trading Journal/Screens/LoginPage.swift
@@ -24,16 +24,70 @@ struct LoginPage: View {
             
             ScrollView(.vertical, showsIndicators: false){
                 VStack(spacing: 15){
-                    Text("Login")
+                    Text(loginData.registeredUser ? "Register" : "Login")
                         .font(.system(size: 25))
                         .fontWeight(.semibold)
                         .foregroundColor(Color.black)
                     
                     //Text field
                     CustomTextField(icon: "envelope", title: "Email", hint: "carl@email.com", value: $loginData.email, showPassword: $loginData.showPassword)
+                        .padding(.top, 10)
+                    CustomTextField(icon: "lock", title: "Password", hint: "Carl1sTh3BestCat", value: $loginData.password, showPassword: $loginData.showPassword)
+                        .padding(.top, 10)
                     
-                    CustomTextField(icon: "lock", title: "Password", hint: "Carl1sTh3BestCat", value: $loginData.email, showPassword: $loginData.showPassword)
+                    //check if user is registered
+                    if loginData.registeredUser{
+                        CustomTextField(icon: "envelope", title: "Re-Enter Password", hint: "I Love You, Riley", value: $loginData.enterPassword, showPassword: $loginData.showEnterPassword)
+                            .padding(.top, 10)
+
+                    }
                     
+                    // forgot password button
+                    Button {
+                        loginData.ForgotPassword()
+                    } label: {
+                        Text("Forgot Password")
+                            .foregroundColor(Color(#colorLiteral(red: 0.04, green: 0.09, blue: 0.05, alpha: 1.00)))
+                            .font(.system(size: 17))
+                            .fontWeight(.bold)
+                    }
+                    .padding(.top, 8)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                    
+                    // Login button
+                    Button {
+                        if loginData.registeredUser{
+                            loginData.Register()
+                        } else{
+                            loginData.Login()
+                        }
+                    } label: {
+                        Text("Login")
+                            .foregroundColor(Color.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 20))
+                            .padding(.vertical, 20)
+                            .frame(maxWidth: .infinity)
+                            .background((Color(#colorLiteral(red: 0.04, green: 0.09, blue: 0.05, alpha: 1.00))))
+                            .cornerRadius(15)
+                            .shadow(color: Color.black.opacity(0.07), radius: 5, x: 5, y: 5)
+
+                    }
+                    .padding(.top, 25)
+                    .padding(.horizontal)
+                    
+                    //sign up button
+                    Button {
+                        withAnimation{
+                            loginData.registeredUser.toggle()
+                        }
+                    } label: {
+                        Text(loginData.registeredUser ?  "Back to Login" : "Sign Up")
+                            .foregroundColor(Color(#colorLiteral(red: 0.04, green: 0.09, blue: 0.05, alpha: 1.00)))
+                            .font(.system(size: 17))
+                            .fontWeight(.bold)
+                    }
+
                 }
                 .padding(45)
             }
@@ -49,30 +103,70 @@ struct LoginPage: View {
         //green bg layer
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 0.04, green: 0.09, blue: 0.05, alpha: 1.00)))
+        
+        
+        // clear data
+        .onChange(of: loginData.registeredUser){
+            newValue in
+            
+            loginData.email = ""
+            loginData.password = ""
+            loginData.enterPassword = ""
+            loginData.showPassword = false
+            loginData.showEnterPassword = false
+        }
     }
     
     @ViewBuilder
-    //text feild for login ingo
+    //text feild for login info
     func CustomTextField (icon: String, title: String, hint: String, value: Binding<String>, showPassword: Binding<Bool>)-> some View{
         VStack(alignment: .leading, spacing: 12){
             
             Label {
                 Text(title)
                     .font(.system(size: 20))
-                    .fontWeight(.medium)
+                    .fontWeight(.semibold)
                 
             } icon: {
                 Image(systemName: icon)
                 
             }
-            .foregroundColor(Color.black.opacity(0.8))
+            .foregroundColor(Color.black.opacity(0.7))
             
-            TextField(hint, text: value)
+            if title.contains("Password") && !showPassword.wrappedValue{
+                //hides the password input
+                SecureField(hint, text: value)
+                    .padding(.top, 2)
+            }
+            else{
+                TextField(hint, text: value)
+                    . padding(.top,2)
+            }
+
             Divider()
+                .background(Color.black.opacity(0.8))
         }
+        //Button to show password
+        .overlay(
+        Group{
+            if title.contains("Password"){
+                Button (action: {
+                    showPassword.wrappedValue.toggle()
+                }, label: {
+                    Text(showPassword.wrappedValue ? "Hide" : "Show")
+                        .foregroundColor(Color(#colorLiteral(red: 0.04, green: 0.09, blue: 0.05, alpha: 1.00)))
+                        .font(.system(size: 17))
+                        .fontWeight(.bold)
+                })
+                    .offset(y: 10)
+            }
+
+        }
+        // moves the show from the middle to side
+        ,alignment: .trailing
+        )
     }
 }
-
 
 struct LoginPage_Previews: PreviewProvider {
     static var previews: some View {
